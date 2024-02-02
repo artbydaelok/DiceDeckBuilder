9e0f41ff1fdb3d6175aa98163178463c7d7dac2c
Daelok <61995345+artbydaelok@users.noreply.github.com>
2024-02-02 00:06:19 -0500

    Added fire boot functionality.

---
+++ abilities/ability_base/ability.gd
@@ -0,0 +1,11 @@
+extends Node
+class_name Ability
+
+@export var ability_name : String
+@export var ability_id : String
+@export var ability_description : String
+@export var ability_icon : Texture2D
+
+@export var consumable : bool
+
+var current_side : int
--- card/inventory.gd
+++ card/inventory.gd
@@ -3,7 +3,7 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	GameEvents.connect("dice_moved", on_dice_moved)
+	GameEvents.dice_moved.connect(on_dice_moved)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
