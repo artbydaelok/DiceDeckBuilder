5366339cd9eccfe9b98de822c500d98364959598
Daelok <61995345+artbydaelok@users.noreply.github.com>
2024-09-25 22:37:09 -0400

    Fixed firewall bug, moved firewalls closer to center to make dodging them more difficult

--- enemy/boss/fire_demon/fire_demon_boss.gd
+++ enemy/boss/fire_demon/fire_demon_boss.gd
@@ -10,6 +10,9 @@ const FIRE_WALL = preload("res://enemy/boss/fire_demon/fire_wall.tscn")
 @onready var fire_wall_spawn_right: Marker3D = $FireWallSpawnRight
 @onready var fire_wall_spawn_left: Marker3D = $FireWallSpawnLeft
 
+
+@onready var damage_animation: AnimationPlayer = $DamageAnimation
+
 func spawn_walls(left_first: bool):
 	if left_first:
 		wall_spawn_anim.play("LeftFirst")
@@ -18,6 +21,7 @@ func spawn_walls(left_first: bool):
 
 func on_damage_taken(damage_amount):
 	print(name + " has been hit for " + str(damage_amount) + " damage!")
+	damage_animation.play("damaged")
 
 func _on_attack_timer_timeout() -> void:
 	spawn_walls(true)
@@ -25,7 +29,7 @@ func _on_attack_timer_timeout() -> void:
 		spawn_meteor()
 		if (current_health / max_health) < 0.3:
 			await get_tree().create_timer(1.0).timeout
-			spawn_meteor()	
+			spawn_meteor()
 
 func spawn_left_wall():
 	var fire_wall = FIRE_WALL.instantiate()
--- enemy/boss/fire_demon/fire_wall.gd
+++ enemy/boss/fire_demon/fire_wall.gd
@@ -1,5 +1,17 @@
 extends Node3D
 
+var speed = 3
+@onready var hitbox_collision: CollisionShape3D = $Hitbox/HitboxCollision
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	global_position += Vector3(0, 0, 2 * delta)
+	global_position += Vector3(0, 0, speed * delta)
+
+func flip_sprite():
+	$FireSpriteFront.flip_h = bool(randi() % 2)
+	$FireSpritBack.flip_h = bool(randi() % 2)
+
+
+func _on_hitbox_area_entered(area: Area3D) -> void:
+	await get_tree().create_timer(0.1).timeout
+	hitbox_collision.disabled = true
