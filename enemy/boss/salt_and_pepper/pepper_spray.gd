a6e2145af186b50e102ee1a796a924e8fd15e58f
Daelok <61995345+artbydaelok@users.noreply.github.com>
2025-01-29 13:38:57 -0500

    Pepper spray now appears when red light is triggered on peppers side, animations have been implemented and state handling

--- enemy/boss/salt_and_pepper/pepper_spray.gd
+++ enemy/boss/salt_and_pepper/pepper_spray.gd
@@ -3,6 +3,20 @@ extends Node3D
 @onready var pepper_anim: Sprite3D = $PepperAnim
 const BULLET_PROJECTILE = preload("res://enemy/boss/salt_and_pepper/bullet_projectile.tscn")
 
+@onready var left_gun: Marker3D = $PepperAnim/LeftGun
+@onready var right_gun: Marker3D = $PepperAnim/RightGun
+
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
+var entities_layer
+signal finished_shooting
+
+var player 
+
+func _ready() -> void:
+	entities_layer = get_tree().get_first_node_in_group("entities_layer")
+	player = get_tree().get_first_node_in_group("player")
+
 func begin_attack():
 	pepper_anim.play("show_guns")
 
@@ -11,7 +25,37 @@ func _on_pepper_anim_animation_finished() -> void:
 	pass # Replace with function body.
 
 func shoot(left_side: bool = false):
-	pass
-
+	var bullet = BULLET_PROJECTILE.instantiate()
+	entities_layer.add_child(bullet)
+	
+	if left_side:
+		bullet.global_position = left_gun.global_position
+		var direction = pepper_anim.global_position.direction_to(left_gun.global_position)
+		bullet.shoot(Vector3(direction.x, 0, direction.z), 12.0)
+	else:
+		bullet.global_position = right_gun.global_position
+		var direction = pepper_anim.global_position.direction_to(right_gun.global_position)
+		bullet.shoot(Vector3(direction.x, 0, direction.z), 12.0)
+		
 func begin_rotation():
-	pepper_anim.rotate_y(PI/8)
+	pepper_anim.rotate_y(PI/5)
+	if pepper_anim.rotation.y < 0.0:
+		disappear()
+
+func appear():
+	animation_player.play("spawn")
+	player.add_blocked_pos(Vector2(0, 0))
+	if player.grid_pos == Vector2(0,0):
+		var rnd_dir = [Vector3.LEFT, Vector3.FORWARD, Vector3.BACK, Vector3.RIGHT].pick_random()
+		player.roll(rnd_dir)
+
+func disappear():
+	animation_player.play("despawn")
+	await animation_player.animation_finished
+	finished_shooting.emit()
+	player.remove_blocked_pos(Vector2(0, 0))
+
+func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+	match anim_name:
+		"spawn":
+			animation_player.play("shots")
--- enemy/boss/salt_and_pepper/salt_and_pepper.gd
+++ enemy/boss/salt_and_pepper/salt_and_pepper.gd
@@ -1,12 +1,85 @@
 extends Enemy
 
-@onready var salt_sprite: AnimatedSprite3D = $SaltSprite
-@onready var pepper_sprite: AnimatedSprite3D = $PepperSprite
 
-@onready var pepper_animations: AnimationPlayer = $PepperAnimations
 
 @export var traffic_light : Node3D
+
+# Salt Variables
+@onready var salt_sprite: AnimatedSprite3D = $SaltSprite
+@export var salt_trigger_area: Area3D
+
+# Pepper Variables
+@onready var pepper_sprite: AnimatedSprite3D = $PepperSprite
+@onready var pepper_animations: AnimationPlayer = $PepperAnimations
 @export var pepper_spray: Node3D
+@export var pepper_trigger_area: Area3D
+
+enum BOSS_STATE {
+	RED_MIDDLE,
+	RED_SALT,
+	RED_PEPPER,
+	
+	GREEN_MIDDLE,
+	GREEN_SALT,
+	GREEN_PEPPER
+}
+
+var boss_state = BOSS_STATE.RED_MIDDLE
+
+func initialize() -> void:
+	salt_trigger_area.body_entered.connect(on_salt_area_triggered)
+	salt_trigger_area.body_exited.connect(on_salt_area_left)
+	pepper_trigger_area.body_entered.connect(on_pepper_area_triggered)
+	pepper_trigger_area.body_exited.connect(on_pepper_area_left)
+	traffic_light.green_light.connect(on_green_light)
+	traffic_light.red_light.connect(on_red_light)
+	
+	pepper_spray.finished_shooting.connect(on_pepper_spray_done)
+	
+func on_green_light():
+	match boss_state:
+		BOSS_STATE.RED_MIDDLE:
+			boss_state = BOSS_STATE.GREEN_MIDDLE
+		BOSS_STATE.RED_SALT:
+			boss_state = BOSS_STATE.GREEN_SALT
+		BOSS_STATE.RED_PEPPER:
+			boss_state = BOSS_STATE.GREEN_PEPPER
+
+func on_red_light():
+	match boss_state:
+		BOSS_STATE.GREEN_MIDDLE:
+			boss_state = BOSS_STATE.RED_MIDDLE
+		BOSS_STATE.GREEN_SALT:
+			boss_state = BOSS_STATE.RED_SALT
+		BOSS_STATE.GREEN_PEPPER:
+			boss_state = BOSS_STATE.RED_PEPPER
+			pepper_animations.play("PepperSpin")
+		
+
+func on_salt_area_triggered(player):
+	if traffic_light.is_green:
+		boss_state = BOSS_STATE.GREEN_SALT
+	else:
+		boss_state = BOSS_STATE.RED_SALT
+
+func on_salt_area_left(player):
+	if traffic_light.is_green:
+		boss_state = BOSS_STATE.GREEN_MIDDLE
+	else:
+		boss_state = BOSS_STATE.RED_MIDDLE
+	
+func on_pepper_area_triggered(player):
+	if traffic_light.is_green:
+		boss_state = BOSS_STATE.GREEN_PEPPER
+	else:
+		boss_state = BOSS_STATE.RED_PEPPER
+
+func on_pepper_area_left(player):
+	if traffic_light.is_green:
+		boss_state = BOSS_STATE.GREEN_MIDDLE
+	else:
+		boss_state = BOSS_STATE.RED_MIDDLE
+
 
 func salt_grenade_attack():
 	salt_sprite.play("throw_bombs")
@@ -14,6 +87,10 @@ func salt_grenade_attack():
 func stop_salt_grenades():
 	salt_sprite.play("default")
 
+func on_pepper_spray_done():
+	pepper_animations.play("PepperSpinBack")
 
 func _on_pepper_animations_animation_finished(anim_name: StringName) -> void:
-	pass # Replace with function body.
+	match anim_name:
+		"PepperSpin":
+			pepper_spray.appear()
