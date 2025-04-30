6e9ecd1e542f58b1f50612d055830eea3bac4cc6
Daelok <61995345+artbydaelok@users.noreply.github.com>
2025-04-30 01:08:05 -0400

    added camera following player

--- levels/camera.gd
+++ levels/camera.gd
@@ -7,13 +7,20 @@ var rng = RandomNumberGenerator.new()
 
 var shake_strength: float = 0.0
 
+var offset : Vector3
+
+@export var target : Node3D
+
 func _ready() -> void:
 	get_tree().get_first_node_in_group("player").player_damaged.connect(apply_shake)
+	offset = global_position
 
 func apply_shake(_damage):
 	shake_strength = random_strength
 
 func _process(delta: float) -> void:
+	if target:
+		global_position = offset + target.global_position
 	if shake_strength > 0:
 		shake_strength = lerpf(shake_strength, 0, shake_fade * delta)
 		var r_offset = random_offset()
