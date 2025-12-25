05e60361a5240aff69c3f28c771b92cb38f369e0
Daelok <61995345+artbydaelok@users.noreply.github.com>
2025-12-25 02:59:39 -0500

    scouting eyeball of DEATH

---
+++ level_mechanics/debug_player_start.gd
@@ -0,0 +1,7 @@
+extends Node3D
+
+@export var disabled := false
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	if disabled: return
+	get_tree().get_first_node_in_group("player").global_position = global_position
