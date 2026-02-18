ac2ec04016f4e981f8b7cfbcf5768f90a46146d0
Daelok <61995345+artbydaelok@users.noreply.github.com>
2026-02-18 18:36:06 -0500

    editing mouse to have a click effect

--- addons/maaacks_game_template/base/scenes/menus/main_menu/custom_cursor.gd
+++ addons/maaacks_game_template/base/scenes/menus/main_menu/custom_cursor.gd
@@ -1,5 +1,11 @@
 extends Sprite2D
 
+@onready var animation_player: AnimationPlayer = $ClickEffect/AnimationPlayer
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	global_position = get_global_mouse_position()
+	if Input.is_action_just_pressed("left_click"):
+		if animation_player.is_playing():
+			animation_player.stop()
+		animation_player.play("clicked")
