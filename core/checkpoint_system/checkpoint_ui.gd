889a4616b722e3e428b6de00eed1d1272401459b
Gausroth <jeske.timothy@yahoo.com>
2026-02-19 13:02:39 -0600

    Checkpoint System UI
    Added the checkpoint system UI.
    Run GausRothTestLevel to test UI.

--- core/checkpoint_system/checkpoint_ui.gd
+++ core/checkpoint_system/checkpoint_ui.gd
@@ -1,4 +1,5 @@
 extends Node
+class_name CheckpointUI
 
 
 # Called when the node enters the scene tree for the first time.
@@ -9,3 +10,7 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _on_button_pressed() -> void:
+	queue_free()
