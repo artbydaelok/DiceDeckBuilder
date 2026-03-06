8919fc58c92ded5543c04d929c84df421d882c5f
Daelok <61995345+artbydaelok@users.noreply.github.com>
2026-03-05 22:15:59 -0500

    Update camera_change_trigger.gd

--- level_mechanics/camera_change_trigger.gd
+++ level_mechanics/camera_change_trigger.gd
@@ -4,6 +4,8 @@ var camera : GameCamera
 
 @export var relocation_node : Node3D
 @export var one_shot : bool
+## This makes the camera stop following the target. 
+@export var static_cam : bool = false
 
 func _ready() -> void:
 	camera = get_tree().get_first_node_in_group("game_camera")
@@ -12,5 +14,9 @@ func _ready() -> void:
 func _on_player_entered(body):
 	var new_offset = relocation_node.global_position - global_position
 	camera.relocate_to(relocation_node, new_offset)
+	if static_cam:
+		camera.follow_target = false
+	else: 
+		camera.follow_target = true
 	if one_shot: queue_free()
 	
