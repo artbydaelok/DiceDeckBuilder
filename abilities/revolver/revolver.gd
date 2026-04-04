072c9d3b999a52af16fc31744303fc497db7b87d
Daelok <61995345+artbydaelok@users.noreply.github.com>
2026-04-04 15:09:21 -0400

    revolver fix
    - revolver fix
    - added revolver sound
    - added revolver sprite

--- abilities/revolver/revolver.gd
+++ abilities/revolver/revolver.gd
@@ -7,20 +7,31 @@ const BULLET_PROJECTILE = preload("uid://4w0fpalmk12o")
 var starting_shots : int = 6
 var current_shots : int = 0
 
+var disabled = false
+
 func initialize():
 	self_destroy_timer.queue_free()
 	player.player_moved.connect(shoot)
 	current_shots = starting_shots
+	
+	var spawn_pos : Vector3 = player.global_position + (Vector3.UP * 2.0)
+	revolver_mesh.global_position = spawn_pos
+
 
 func shoot(player_moved_direction : Vector3):
+	if disabled : return
 	current_shots -= 1
 	var bullet : BulletProjectile = BULLET_PROJECTILE.instantiate()
 	var spawn_pos : Vector3 = player.global_position + (Vector3.UP * 2.0)
 	revolver_mesh.global_position = spawn_pos
 	revolver_mesh.animate_shoot()
-	await get_tree().create_timer(0.4).timeout # Currently this is hard-coded, but ideally we could call this from the animation itself, maybe? as a signal
+	
+	await revolver_mesh.anim_shot_frame # Signal emitted/called from animation player
+	
 	entities_layer.add_child(bullet)
 	var bullet_pos = revolver_mesh.get_bullet_spawn_position()
 	bullet.setup(5, true, bullet_pos, Vector3.FORWARD, 12.0)
-	if current_shots == 0:
+	if current_shots <= 0:
+		disabled = true
+		await get_tree().create_timer(1.0).timeout
 		queue_free()
--- abilities/revolver/revolver_mesh.gd
+++ abilities/revolver/revolver_mesh.gd
@@ -3,6 +3,8 @@ extends Node3D
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 @onready var bullet_spawn_marker: Marker3D = $BulletSpawnMarker
 
+signal anim_shot_frame
+
 func animate_shoot():
 	animation_player.play("shoot")
 
